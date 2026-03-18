5b892875547656925fa657ffde3f0744e1129f2f
c5042092 <Victor.O.Abbah@student.shu.ac.uk>
2026-03-18 09:28:31 +0000

    Added Create and Update forms with basic validation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_wtf import CSRFProtect
 from flask_wtf.csrf import generate_csrf
+from db.db import get_all_films, get_film_by_id
+from db.db import get_all_films, get_film_by_id, create_film, update_film, delete_film
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -94,6 +96,102 @@ def login():
 
     # If the request method is GET, render the login form
     return render_template("login.html", title="Log In")
+
+@app.route('/films')
+def films():
+    film_list = get_all_films() 
+    # Render the films.html template with a list of films
+    return render_template('films.html', title="All Films", films=film_list)
+
+# Film Detail Page
+@app.route('/film/<int:id>/')
+def film(id):
+    # Get film data
+    film_data = get_film_by_id(id) 
+    if film_data:
+        return render_template('film.html', title=film_data['title'], film=film_data)
+    else:
+        # If film not found, redirect to films list with a flash message
+        flash(category='warning', message='Requested film not found!')
+        return redirect(url_for('films'))
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    # If the request method is POST, process the form submission
+    if request.method == 'POST':
+        # Get the title input from the form
+        title = request.form['title']
+        # Validate the input
+        if not title:
+            flash(category='danger', message='Title is required!')
+            return render_template('create.html')
+        
+        # [TO-DO]: Add real creation logic here (e.g. save to database record)
+
+        new_film = {
+            'user': 1, # test user
+            'title': title,
+            'tagline': request.form.get('tagline', ''),
+            'director': request.form.get('director', ''),
+            'poster': request.form.get('poster', ''),
+            'release_year': request.form.get('release_year', 0),
+            'genre': request.form.get('genre', ''),
+            'watched': 'watched' in request.form,
+            'rating': request.form.get('rating', 0),
+            'review': request.form.get('review', '')
+        }
+        create_film(new_film)
+        # ===========================
+        # Flash a success message
+        flash(category='success', message='Created successfully!')
+        return redirect(url_for('films'))
+    return render_template('create.html', title="Add A New Film")
+
+@app.route('/update/<int:id>/', methods=('GET', 'POST'))
+def update(id):
+    # Get film data
+    film_data = get_film_by_id(id)
+    if not film_data:
+        flash('Film not found!', 'warning')
+        return redirect(url_for('films'))
+    
+    if request.method == 'POST':
+        # Get the title input from the form
+        title = request.form['title']
+        # Validate the input
+        if not title:
+            flash(category='danger', message='Title is required!')
+            return render_template('update.html', id=id)
+        
+        # [TO-DO]: Add real update logic here (e.g. update database record)
+
+        updated_fields = {
+            'title': title,
+            'tagline': request.form.get('tagline', ''),
+            'director': request.form.get('director', ''),
+            'poster': request.form.get('poster', ''),
+            'release_year': request.form.get('release_year', 0),
+            'genre': request.form.get('genre', ''),
+            'watched': 'watched' in request.form,
+            'rating': request.form.get('rating', 0),
+            'review': request.form.get('review', '')
+        }
+        update_film(id, updated_fields)
+        # ===========================
+        # Flash a success message
+        flash(category='success', message='Updated successfully!')
+        return redirect(url_for('film', id=id))
+    return render_template('update.html', title="Update Film", film=film_data)
+
+@app.route('/delete/<int:id>', methods=('POST',))
+def delete(id):
+    # [TO-DO]: Add real deletion logic here (e.g. delete database record)
+    delete_film(id)
+    # ===========================
+    # Flash a success message and redirect to the index page
+    flash(category='success', message='Film deleted successfully!')
+    return redirect(url_for('films'))
+
 
 # Run application
 #=========================================================
